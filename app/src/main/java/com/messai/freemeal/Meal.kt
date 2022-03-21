ced6a8ae73eeb66cbbf25f74a9cc8ec1fe876a5e
Zakaria MESSAI <messai.zackaria@gmail.com>
2022-03-21 15:10:03 +0100

    body of class Meal

--- app/src/main/java/com/messai/freemeal/Meal.kt
+++ app/src/main/java/com/messai/freemeal/Meal.kt
@@ -1,4 +1,18 @@
 package com.messai.freemeal
 
-class Meal {
-}
+data class Meal (
+    val  id: Long,
+    val title: String,
+    val drinkAlternance: String?,
+    val category: String,
+    val area: String,
+    val instructions: String,
+    val mealThumb: String,
+    val tags: List<String>,
+    val youtubeLink: String,
+    val ingredients: Map<String, String>,
+    val source: String,
+    val imageSrc: String,
+    val creativeCommonsConfirmed: String,
+    val dateModified: String
+)
